d3971b5f15030c1ef5adecda0c97c776a7bd9ebb
Alexandre <alex.4492@gmail.com>
2013-11-19 15:07:31 +0100

    Insertion de l'ethernet
    J'ajoute les librairies pour la pile TCPIP et mon fichier ethernet.c et
    ethernet.h

---
+++ ethernet.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   ethernet.h
+ * Author: etu18666
+ *
+ * Created on 19 novembre 2013, 14:20
+ */
+
+#ifndef ETHERNET_H
+#define	ETHERNET_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* ETHERNET_H */
+
