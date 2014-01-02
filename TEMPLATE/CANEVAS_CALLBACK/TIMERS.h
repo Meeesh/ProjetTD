6f73f3fd540a5afd20fad99db6e5115f87289c53
Meeesh <wery.michael@gmail.com>
2014-01-02 16:20:25 +0100

    Création du projet final

--- TEMPLATE/CANEVAS_CALLBACK/TIMERS.h
+++ TEMPLATE/CANEVAS_CALLBACK/TIMERS.h
@@ -10,7 +10,7 @@
 
 //PROTOTYPE FONCTIONS EXTERNES
 //Création, configuration et démarrage de Timer1 pour générer une interruption toutes les mS en priorité haute
-void TIMER1_Init_1ms(void)
+void TIMER1_Init_1ms(void);
 
 
 #endif /* _TIMERS_H */
